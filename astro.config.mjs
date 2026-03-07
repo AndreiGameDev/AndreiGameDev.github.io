b312e1f0886f4c867abddba6f4bf88550856b5b3
Andrei <49598916+AndreiGameDev@users.noreply.github.com>
2026-03-07 16:07:51 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,6 +41,7 @@ import PlayformInline from "@playform/inline";
 export default defineConfig({
   site: "https://github.com/AndreiGameDev",
   base: "/portfolio/", // must include trailing slash for correct asset and routing on GitHub Pages
+  trailingSlash: "always", // ensures /foo/ maps to /foo/index.html for GitHub Pages
   build: {
     format: "directory",
   },
